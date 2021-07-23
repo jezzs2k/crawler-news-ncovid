ab1b116d1c1a61585f0c765464a18bacf760872d
Vu Thanh Hieu <vuthanhhieu00@gmail.com>
2021-07-23 16:57:48 +0700

    update request /news

--- index.js
+++ index.js
@@ -11,6 +11,30 @@ app.get('/', (req, res) => {
   res.send('Hello my friend !')
 })
 
+app.get('/news', (req, res) => {
+    const pageIndex = 1;
+    
+    fs.readFile('news.json', 'utf8', function readFileCallback(err, data) {
+        if (err) {
+            return res.send('Error', + JSON.stringify(err))
+        }else {
+            if (data) {
+                const newsFile = JSON.parse(data);
+
+                if (!newsFile?.news) {
+                    return res.send('News is empty !');
+                }else {
+                    const newsData = newsFile.news;
+
+                    return res.jsonp(newsData.slice((pageIndex-1)*20, pageIndex*20))
+                }
+            }else {
+                return res.send('News is empty !');
+            }
+        }
+    })
+})
+
 app.get('/news/:page', function (req, res) {
     const pageIndex = req.params.page;
     fs.readFile('news.json', 'utf8', function readFileCallback(err, data) {
